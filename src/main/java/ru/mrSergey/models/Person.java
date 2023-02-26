a1f71039e9fd2fc8349a05ef453771ddb8e69ab3
Mr.Sergey <Forex187000@gmail.com>
2023-02-26 19:09:03 +0300

    Library

---
+++ src/main/java/ru/mrSergey/models/Person.java
@@ -0,0 +1,42 @@
+package ru.mrSergey.models;
+
+import jakarta.validation.constraints.*;
+
+public class Person {
+    private int id;
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+    private int year;
+
+    public Person(int id, String fio, int year) {
+        this.id = id;
+        this.name = fio;
+        this.year = year;
+    }
+    public Person(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
